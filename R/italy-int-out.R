aa0e0ec0b3987444128280957bd4e7d9f2a9b864
ctaglioni <ctaglioni@github.com>
2018-01-18 15:25:49 +1300

    Error in Roxygen comment

--- R/italy-int-out.R
+++ R/italy-int-out.R
@@ -2,7 +2,7 @@
 #' italian region per departing region, sex, large age groups,
 #' year 2006-2016.
 #'
-@format An array with dimensions "age", "sex", "region" and "year".
+#' @format An array with dimensions "age", "sex", "region" and "year".
 #' Age groups are "0-17", "18-39", "40-64", "65+".
 #'
 #' @source Custom tabulation from Italian Statistical Institute (ISTAT)
