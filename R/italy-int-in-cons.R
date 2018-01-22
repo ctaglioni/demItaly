58193b88cd41dea3824bd4c972063ca0f084fb74
ctaglioni <ctaglioni@github.com>
2018-01-22 13:41:50 +1300

    documentation for consistent migr data

---
+++ R/italy-int-in-cons.R
@@ -0,0 +1,13 @@
+#' New registrations of people coming from other italian regions
+#' per region of destination, sex, large age groups, year 2006-2016.
+#' Data have been modified in order to have consistent net migration,
+#' i.e. in the country the total number of cancellations equals
+#' the total number of enrollement.
+#'
+#' @format An array with dimensions "age", "sex", "region" and "year".
+#' Age groups are "0-17", "18-39", "40-64", "65+".
+#'
+#' @source Custom tabulation from Italian Statistical Institute (ISTAT)
+#' \url{http://dati.istat.it/}.
+
+"italy.int.in.cons"
---
+++ R/italy-int-out-cons.R
@@ -0,0 +1,14 @@
+#' Cancellations of people leaving from an italian region to any other
+#' italian region per departing region, sex, large age groups,
+#' year 2006-2016.
+#' Data have been modified in order to have consistent net migration,
+#' i.e. in the country the total number of cancellations equals
+#' the total number of enrollement.
+#'
+#' @format An array with dimensions "age", "sex", "region" and "year".
+#' Age groups are "0-17", "18-39", "40-64", "65+".
+#'
+#' @source Custom tabulation from Italian Statistical Institute (ISTAT)
+#' \url{http://dati.istat.it/}.
+
+"italy.int.out.cons"
